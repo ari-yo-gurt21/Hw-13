1b57c62f884e06d6ad8475f8cb116979e79057a7
Ari Lee <Theboss21ari@gmail.com>
2025-11-18 18:04:10 -0800

    Initial Commit

---
+++ Hw 13/Hw_13App.swift
@@ -0,0 +1,17 @@
+//
+//  Hw_13App.swift
+//  Hw 13
+//
+//  Created by Ari Lee on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Hw_13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hw 13Tests/Hw_13Tests.swift
@@ -0,0 +1,16 @@
+//
+//  Hw_13Tests.swift
+//  Hw 13Tests
+//
+//  Created by Ari Lee on 11/18/25.
+//
+
+import Testing
+
+struct Hw_13Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
